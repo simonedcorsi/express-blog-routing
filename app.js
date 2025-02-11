af8cd7b374620b1cafbf4b238c1776e7a6d5e688
simonedcorsi <simonedomenico.corsi@gmail.com>
2025-02-11 15:27:04 +0100

    create CRUD

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const ricetteRouter = require('./routers/posts.js');
 
 app.use(express.static('public'));
 
@@ -8,45 +9,53 @@ app.get('/', (req, res) => {
   res.send('Server del mio blog');
 });
 
+app.use("/api/posts", ricetteRouter)
+
+// app.get("/api/bacheca", (req, res) => {
+
+//     const posts = [
+//         {
+//             title: 'Ciambellone',
+//             content: 'imaggine ciambellone',
+//             image: '/images/ciambellone.jpg',
+//             tags: ['ricette', 'piatti tipici']
+//         },
+//         {
+//             title: 'Cracker_barbabietole',
+//             content: 'immagine cracker',
+//             image: '/images/cracker_barbabietola.jpg',
+//             tags: ['ricette', 'piatti tipici']
+//         },
+//         {
+//             title: 'Pane_fritto_dolce',
+//             content: 'immagine pane',
+//             image: '/images/pane_fritto_dolce.jpg',
+//             tags: ['ricette', 'piatti tipici']
+//         },
+//         {
+//             title: 'Pasta_barbabietola',
+//             content: 'immagine pasta',
+//             image: '/images/pasta_barbabietola.jpg',
+//             tags: ['ricette', 'piatti tipici']
+//         },
+//         {
+//             title: 'Torta_paesana',
+//             content: 'immagine torta',
+//             image: '/images/torta_paesana.jpg',
+//             tags: ['ricette', 'piatti tipici']
+//         }
+//     ];
+//  res.json(posts);
+// })    
+
+// app.get('/api/bachceca/:id', (req, res) => {
+//          res.send(req.params.id);
+    
+    
+//  });
+
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
-});
-
-app.get("/api/bacheca", (req, res) => {
-
-    const posts = [
-        {
-            title: 'Ciambellone',
-            content: 'imaggine ciambellone',
-            image: '/images/ciambellone.jpg',
-            tags: ['ricette', 'piatti tipici']
-        },
-        {
-            title: 'Cracker_barbabietole',
-            content: 'immagine cracker',
-            image: '/images/cracker_barbabietola.jpg',
-            tags: ['ricette', 'piatti tipici']
-        },
-        {
-            title: 'Pane_fritto_dolce',
-            content: 'immagine pane',
-            image: '/images/pane_fritto_dolce.jpg',
-            tags: ['ricette', 'piatti tipici']
-        },
-        {
-            title: 'Pasta_barbabietola',
-            content: 'immagine pasta',
-            image: '/images/pasta_barbabietola.jpg',
-            tags: ['ricette', 'piatti tipici']
-        },
-        {
-            title: 'Torta_paesana',
-            content: 'immagine torta',
-            image: '/images/torta_paesana.jpg',
-            tags: ['ricette', 'piatti tipici']
-        }
-    ];
- res.json(posts);
-})    
+    console.log(`Example app listening on port ${port}`);
+  });
     
 
---
+++ routers/posts.js
@@ -0,0 +1,32 @@
+const express = require('express')
+const router = express.Router();
+
+// index
+router.get('/', function (req, res) {
+    res.send('Lista delle ricette');
+});
+// show
+router.get('/:id', function (req, res) {
+    res.send('Dettagli della ricetta ' + req.params.id);
+});
+// store
+router.post('/', function (req, res) {
+    res.send('Creazione nuova ricetta');
+});
+
+// update
+router.put('/:id', function (req, res) {
+    res.send('Modifica integrale della ricetta ' + req.params.id);
+});
+
+// modify
+router.patch('/:id', function (req, res) {
+    res.send('Modifica parziale della ricetta ' + req.params.id);
+});
+
+// destroy
+router.delete('/:id', function (req, res) {
+    res.send('Eliminazione della ricetta ' + req.params.id);
+});
+
+module.exports = router;
